5af304bd2aa4db147e8c2493d33b8ea88c5a6c53
jochongs <joch2712@naver.com>
2024-01-24 13:48:04 +0900

    docs: add jsDoc in StringValidator

--- lib/class/validate/StringValidator.ts
+++ lib/class/validate/StringValidator.ts
@@ -23,7 +23,6 @@ export class StringValidator extends Validator {
 
   /**
    * A method of evaluating whether or not it is in hanguel
-   * @param option default value of complete is false, default value of space is false
    */
   isHanguel(option: Validate.String.IsHanguelOption) {
     this.taskList.push(new Task(isHangeul, option));
@@ -31,36 +30,54 @@ export class StringValidator extends Validator {
     return this;
   }
 
+  /**
+   * Method for determining the length of an array
+   */
   length(option: Validate.String.LengthOption) {
     this.taskList.push(new Task(length, option));
 
     return this;
   }
 
+  /**
+   * Method of verifying that the entered value satisfies a regular expression
+   */
   match(regExp: RegExp) {
     this.taskList.push(new Task(match, regExp));
 
     return this;
   }
 
+  /**
+   * Method to verify that it contains only alphabets
+   */
   isOnlyAlphabet() {
     this.taskList.push(new Task(isOnlyAlphabet));
 
     return this;
   }
 
+  /**
+   * Method to determine if it is in date format
+   */
   isDate() {
     this.taskList.push(new Task(isDate));
 
     return this;
   }
 
+  /**
+   * Method to determine if it is in date format containing time
+   */
   isDateTime() {
     this.taskList.push(new Task(isDateTime));
 
     return this;
   }
 
+  /**
+   * Method of verifying that it is in password format
+   */
   isPw(option?: Validate.String.IsPwOption) {
     this.taskList.push(new Task(isPw, option));
 
