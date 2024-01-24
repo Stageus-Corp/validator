9fe4498e21ce155ea8384526a6d31923e43d154c
jochongs <joch2712@naver.com>
2024-01-24 13:42:50 +0900

    docs: add jsDoc

--- lib/class/validate/ArrayValidator.ts
+++ lib/class/validate/ArrayValidator.ts
@@ -10,18 +10,29 @@ export class ArrayValidator extends Validator {
     super(message, taskList);
   }
 
+  /**
+   * Method to check the length of an array
+   * @returns
+   */
   legnth(option: Validate.Array.LengthOption) {
     this.taskList.push(new Task(length, option));
 
     return this;
   }
 
+  /**
+   * Method for check that all elements in an array are not null
+   */
   isAllNotNull() {
     this.taskList.push(new Task(isAllNotNull));
 
     return this;
   }
 
+  /**
+   * A method that allows you to create a custom function to inspect elements in array directly
+   * @param func A function that determines whether an element of an array is true or false when it enters a function
+   */
   isAllIf(func: (element: any) => boolean) {
     this.taskList.push(new Task(isAllIf, func));
 
