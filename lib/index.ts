59e81386baf3538b35b31f6d8234290a9c6b56a9
jochongs <joch2712@naver.com>
2024-02-04 18:37:51 +0900

    refacotr: change function name validator to message

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
 import { TypeValidator } from './class/validate/TypeValidator';
 
-export const validator = (message: string | null = null) => {
+export const message = (message: string | null = null) => {
   return new TypeValidator(message);
 };
