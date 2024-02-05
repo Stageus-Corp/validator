5051c1fc6eced5accf13865dbbbb5f53808aa1af
jochongs <joch2712@naver.com>
2024-02-05 16:24:18 +0900

    refactor: modify isEmail to use validator library

--- lib/method/validate/string/isEmail.ts
+++ lib/method/validate/string/isEmail.ts
@@ -1,10 +1,9 @@
 import { TaskResult } from '../../../class/TaskResult';
 import { ValidateMethod } from '../../../../types/ValidateMethod';
+import validator from 'validator';
 
 export const isEmail: ValidateMethod<string> = (value) => {
-  const emailRegExp = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-
-  const condition = emailRegExp.test(value);
+  const condition = validator.isEmail(value);
   if (!condition) {
     return new TaskResult(false, value, `Value is not email format`);
   }
