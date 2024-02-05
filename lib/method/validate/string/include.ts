93dbd496e86d6892eb9fa358adfc0e12e0eafee5
jochongs <joch2712@naver.com>
2024-02-05 14:30:07 +0900

    refactor: modify include method to use validator library

--- lib/method/validate/string/include.ts
+++ lib/method/validate/string/include.ts
@@ -1,8 +1,9 @@
 import { TaskResult } from '../../../class/TaskResult';
 import { ValidateMethod } from '../../../../types/ValidateMethod';
+import validator from 'validator';
 
 export const include: ValidateMethod<string> = (value, includeStr: string) => {
-  const condition = value.includes(includeStr);
+  const condition = validator.contains(value, includeStr);
   if (!condition) {
     return new TaskResult(
       false,
