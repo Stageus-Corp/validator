91be77b545a93ea03dcffa13ecbff75c0743407c
jochongs <joch2712@naver.com>
2024-02-05 16:05:47 +0900

    feat: add taskGenerator method and isAllJwt method

--- lib/class/validate/StringArrayValidator.ts
+++ lib/class/validate/StringArrayValidator.ts
@@ -1,11 +1,32 @@
 import { isAllMatch } from '../../method/validate/stringArray/isAllMatch';
-import { Validate } from '../../../types/Validate';
 import { Task } from '../Task';
 import { Validator } from './Validator';
+import { ValidateMethod } from '../../../types/ValidateMethod';
+import { TaskResult } from '../TaskResult';
+import { isJwt } from '../../method/validate/string/isJwt';
 
 export class StringArrayValidator extends Validator {
   constructor(message: null | string = null, taskList: Task[] = []) {
     super(message, taskList);
+  }
+
+  private taskGenerator(
+    method: ValidateMethod,
+    message: string,
+    ...arg: any[]
+  ) {
+    return new Task((value, ...arg) => {
+      for (const i in value) {
+        const element = value[i];
+
+        const condition = method(element, ...arg).valid;
+        if (!condition) {
+          return new TaskResult(false, value, `Value(Array[${i}]) ` + message);
+        }
+      }
+
+      return new TaskResult(true, value);
+    }, ...arg);
   }
 
   /**
@@ -13,6 +34,12 @@ export class StringArrayValidator extends Validator {
    */
   isAllMatch(regExp: RegExp) {
     this.taskList.push(new Task(isAllMatch, regExp));
+
+    return this;
+  }
+
+  isAllJwt() {
+    this.taskList.push(this.taskGenerator(isJwt, 'is not jwt'));
 
     return this;
   }
