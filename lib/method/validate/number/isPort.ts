9843d03e2cd17b91248f3a152abc26dcbe7884c0
jochongs <joch2712@naver.com>
2024-02-05 14:16:40 +0900

    refactor: modify isPort to use validator library

--- lib/method/validate/number/isPort.ts
+++ lib/method/validate/number/isPort.ts
@@ -1,17 +1,11 @@
 import { TaskResult } from '../../../class/TaskResult';
 import { ValidateMethod } from '../../../../types/ValidateMethod';
-import { isInt } from './isInt';
-import { range } from './range';
+import validator from 'validator';
 
 export const isPort: ValidateMethod<number> = (value) => {
-  const intCondition = isInt(value);
-  if (!intCondition.valid) {
-    return new TaskResult(false, value, 'Value is not integer');
-  }
-
-  const portCondition = range(value, { min: 0, max: 65535 });
-  if (!portCondition.valid) {
-    return new TaskResult(false, value, 'Value cannot be port number');
+  const condition = validator.isPort(value.toString());
+  if (!condition) {
+    return new TaskResult(false, value, 'Value is not port number');
   }
 
   return new TaskResult(true, value);
