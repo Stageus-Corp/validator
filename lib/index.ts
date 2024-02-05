209c338e68e175916932b75fbe4086103b2a09f6
jochongs <joch2712@naver.com>
2024-02-05 16:49:58 +0900

    feat: modify main function name to message

--- lib/index.ts
+++ lib/index.ts
@@ -3,11 +3,3 @@ import { TypeValidator } from './class/validate/TypeValidator';
 export const message = (message: string | null = null) => {
   return new TypeValidator(message);
 };
-
-console.log(
-  message()
-    .isArray()
-    .isAllString()
-    .isAllStartWith('1')
-    .run(['223', '22344', '2'])
-);
