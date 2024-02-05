ac56692785f74ab6bd464e8cda22e4617b6c00f5
jochongs <joch2712@naver.com>
2024-02-05 16:42:15 +0900

    feat: add NumberArrayValidator methods

--- lib/class/validate/NumberArrayValidator.ts
+++ lib/class/validate/NumberArrayValidator.ts
@@ -1,4 +1,10 @@
+import { Validate } from '../../../types/Validate';
 import { ValidateMethod } from '../../../types/ValidateMethod';
+import { isIn } from '../../method/validate/number/isIn';
+import { isInt } from '../../method/validate/number/isInt';
+import { isNotIn } from '../../method/validate/number/isNotIn';
+import { isPort } from '../../method/validate/number/isPort';
+import { range } from '../../method/validate/number/range';
 import { Task } from '../Task';
 import { TaskResult } from '../TaskResult';
 import { Validator } from './Validator';
@@ -25,5 +31,52 @@ export class NumberArrayValidator extends Validator {
 
       return new TaskResult(true, value);
     }, ...arg);
+  }
+
+  isAllInt() {
+    this.taskList.push(this.taskGenerator(isInt, 'is not integer'));
+
+    return this;
+  }
+
+  isAllIn(numberList: number[]) {
+    this.taskList.push(
+      this.taskGenerator(
+        isIn,
+        `is not included in [${numberList.toString()}]`,
+        numberList
+      )
+    );
+
+    return this;
+  }
+
+  isAllNotIn(numberList: number[]) {
+    this.taskList.push(
+      this.taskGenerator(
+        isNotIn,
+        `is included in [${numberList.toString()}]`,
+        numberList
+      )
+    );
+
+    return this;
+  }
+
+  isAllPort() {
+    this.taskList.push(this.taskGenerator(isPort, 'is not port number'));
+
+    return this;
+  }
+
+  allRange(option: Validate.Number.RangeOption) {
+    const min = option.min === undefined ? -Infinity : option.min;
+    const max = option.max === undefined ? +Infinity : option.max;
+
+    this.taskList.push(
+      this.taskGenerator(range, `is out of range ( ${min} ~ ${max} )`, option)
+    );
+
+    return this;
   }
 }
