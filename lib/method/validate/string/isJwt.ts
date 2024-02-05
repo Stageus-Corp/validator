e9641b8ef9152a9fb336e3e4f4c7864ce9fe9210
jochongs <joch2712@naver.com>
2024-02-05 14:46:53 +0900

    refactor: modify isJWT to use validator library

--- lib/method/validate/string/isJwt.ts
+++ lib/method/validate/string/isJwt.ts
@@ -1,20 +1,10 @@
 import { TaskResult } from '../../../class/TaskResult';
 import { ValidateMethod } from '../../../../types/ValidateMethod';
+import validator from 'validator';
 
 export const isJwt: ValidateMethod<string> = (value) => {
-  const [header, payload, signature] = value.split('.');
-
-  try {
-    const decodedHeader = atob(header);
-    const decodedPayload = atob(payload);
-
-    JSON.parse(decodedHeader);
-    JSON.parse(decodedPayload);
-  } catch (err) {
-    return new TaskResult(false, value, 'Value is not jwt');
-  }
-
-  if (!signature) {
+  const condition = validator.isJWT(value);
+  if (!condition) {
     return new TaskResult(false, value, 'Value is not jwt');
   }
 
