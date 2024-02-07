46844c7a6c6ff03ed1b49c859d3085a41b034e55
jochongs <joch2712@naver.com>
2024-02-08 02:27:30 +0900

    feat: change typeof Reason.field to just string not null

--- types/ValidateShema.d.ts
+++ types/ValidateShema.d.ts
@@ -1,7 +1,7 @@
 export namespace ValidateSchema {
   export interface Reason {
     message: string | null;
-    field: string | null;
+    field: string;
   }
 
   export type Callback<T> = (value?: T, reason?: Reason[]) => any;
